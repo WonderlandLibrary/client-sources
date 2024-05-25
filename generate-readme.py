3f6299227afd6a4c7fc931b89a0b25fa192056d0
Párko-pes <144590047+jezevcik@users.noreply.github.com>
2024-05-25 14:14:43 +0200

    Added available list

--- generate-readme.py
+++ generate-readme.py
@@ -5,11 +5,17 @@ template = open("README.template", "r").read()
 output = template
 
 sources_out = ""
+available_out = ""
 for source_name in os.listdir("sources"):
     sources_out += f"- {source_name}\n"
+    available_out += f"{source_name}\n"
 
 output = output.replace("sources-here", sources_out)    
 
-f = open("README.md", "w")
-f.write(output)
-f.close()
+readme = open("README.md", "w")
+readme.write(output)
+readme.close()
+
+available = open("available", "w")
+available.write(available_out)
+available.close()
